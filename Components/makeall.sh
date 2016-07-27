5249db492cfac64027b9360657fe8f076f524c70
Michael Uman <Michael_Uman@sigmadesigns.com>
2016-07-27 13:46:17 -0700

    Check in binary libraries libgbusfifo
    * Added master makeall.sh script for components
    * Check in gbusfifo library binaries.

---
+++ Components/makeall.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+###############################################################################
+#	Build all components
+###############################################################################
+
+COMPONENTS="gbusfifolib platformDB remote_client structure_control symbolmgr"
+
+for comp in $COMPONENTS; do
+	pushd > /dev/null $comp
+	echo "Building $comp"
+	./makeall.sh
+	popd > /dev/null
+done
+
