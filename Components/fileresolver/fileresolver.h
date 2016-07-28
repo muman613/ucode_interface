ca7f043e5bcfd4e52d91ab0ca23bb68922891cbc
Michael Uman <Michael_Uman@sigmadesigns.com>
2016-07-27 19:07:33 -0700

    Added fileresolver

---
+++ Components/fileresolver/fileresolver.h
@@ -0,0 +1,27 @@
+#ifndef __FILERESOLVER_H__
+#define __FILERESOLVER_H__
+
+#include <string>
+
+class FILE_PACK {
+public:
+    void            dump(FILE* oFP);
+
+    std::string     sBinFile;
+    std::string     sLabelFile;
+    std::string     sMapFile;
+    std::string     sListFile;
+    std::string     sInterfaceFile;
+
+    bool            resolve_package(std::string sChildID,
+                                    bool bDebug,
+                                    std::string sPrefix = "");
+
+    bool            isvalid();
+};
+
+bool resolve_package(std::string sChipId, bool bDebug,
+                     FILE_PACK& pack, std::string sPrefix = "");
+
+#endif // __FILERESOLVER_H__
+
--- Components/makeall.sh
+++ Components/makeall.sh
@@ -3,7 +3,7 @@
 #	Build all components
 ###############################################################################
 
-COMPONENTS="gbusfifolib platformDB remote_client structure_control symbolmgr"
+COMPONENTS="gbusfifolib platformDB remote_client structure_control symbolmgr fileresolver"
 
 for comp in $COMPONENTS; do
 	pushd > /dev/null $comp
