0f627e53a7b92008a11e9c556e3237b4c2c41279
Michael Uman <Michael_Uman@sigmadesigns.com>
2016-08-09 17:28:38 -0700

    Dump unions to HTML.
    * Change all D3 debug macros to D for the time being.
    * Dump unions as well as structures to HTML.

--- Components/structure_control/structure_control.cpp
+++ Components/structure_control/structure_control.cpp
@@ -426,7 +426,7 @@ bool structure_database::parse_file(const char* filename) {
 
 //	--	If file ends in '.h'
 
-        D3(debug("-- extension %s\n", ext));
+        D(debug("-- extension %s\n", ext));
 
 		if (strcmp(ext, ".hh") == 0) {
 			bUsePipe = false;
@@ -454,7 +454,7 @@ bool structure_database::parse_file(const char* filename) {
 			return false;
 		}
 
-		//D3(debug("-- piping %s through C preprocessor...\n", filename));
+		//D(debug("-- piping %s through C preprocessor...\n", filename));
 
 		strncpy(RMCFLAGS, env, 256);
 
@@ -499,7 +499,7 @@ bool structure_database::parse_file(const char* filename) {
 				break;
 			}
 
-//			//D3(debug("line > %s", lineptr));
+//			//D(debug("line > %s", lineptr));
 
 			if ((*lineptr == '\n') || (lineptr[0] == '#')) {
 				continue;
@@ -513,7 +513,7 @@ bool structure_database::parse_file(const char* filename) {
 
             if (flags == FLAGS_NONE) {
 
-//                D3(debug("A\n"));
+//                D(debug("A\n"));
 
                 if (regexec(&exp_startstruct, lineptr, 2, matches, 0) == 0) {
                     char*	struct_name;
@@ -524,14 +524,14 @@ bool structure_database::parse_file(const char* filename) {
                     struct_name[len ] 	= 0;
 
                     strncpy(struct_name, lineptr + matches[1].rm_so, len );
-                    D3(debug( "START STRUCT %s:\n", struct_name));
+                    D(debug( "START STRUCT %s:\n", struct_name));
 
                     /* do not add if structure already exists */
                     if (get_structure(struct_name) == 0) {
                         pStruct = new_structure(struct_name);
                         push_state(FLAGS_IN_STRUCT, (void*)pStruct);
                     } else {
-                        D3(debug("Duplicate structure [%s]\n", struct_name));
+                        D(debug("Duplicate structure [%s]\n", struct_name));
                     }
 
                     delete [] struct_name;
@@ -546,14 +546,14 @@ bool structure_database::parse_file(const char* filename) {
 
                     strncpy(union_name, lineptr + matches[1].rm_so, len );
 
-                    D3(debug("START UNION %s\n", union_name));
+                    D(debug("START UNION %s\n", union_name));
 
                     /* do not add if union already exists */
                     if (get_union(union_name) == 0) {
                         pUnion = new_union(union_name);
                         push_state(FLAGS_IN_UNION, (void*)pUnion);
                     } else {
-                        D3(debug("Duplicate union [%s]\n", union_name));
+                        D(debug("Duplicate union [%s]\n", union_name));
                     }
 
                     delete [] union_name;
@@ -564,10 +564,10 @@ bool structure_database::parse_file(const char* filename) {
             if (FLAG_ISSET(flags, FLAGS_IN_STRUCT) || FLAG_ISSET(flags, FLAGS_IN_UNNAMEDSTRUCT) || FLAG_ISSET(flags, FLAGS_IN_UNION) || FLAG_ISSET(flags, FLAGS_IN_UNNAMEDUNION)) {
                 char	member_type[1024], member_name[1024], member_index[1024];
 
-//                D3(debug("B\n"));
+//                D(debug("B\n"));
 
                 if (regexec(&exp_endstruct, lineptr, 0, NULL, 0) == 0) {
-                    D3(debug("-- end of structure or union!\n"));
+                    D(debug("-- end of structure or union!\n"));
                     pop_state();
                 } else if ((FLAG_ISSET(flags, FLAGS_IN_UNNAMEDSTRUCT) || FLAG_ISSET(flags, FLAGS_IN_UNNAMEDUNION))
                         && regexec(&exp_end_unnamed_struct, lineptr, 2, matches, 0) == 0) {
@@ -580,19 +580,19 @@ bool structure_database::parse_file(const char* filename) {
 
                     pop_state(0, (void**)&unnamed_struct_def);
 
-                    D3(debug("-- end of unnamed struct [%s] %s!\n", unnamed_struct_def->name().c_str(), member_name));
+                    D(debug("-- end of unnamed struct [%s] %s!\n", unnamed_struct_def->name().c_str(), member_name));
                     nextFlag = peek_state(&pConstruct);
 
                     assert(pConstruct != 0L);
                     if (FLAG_ISSET(nextFlag, FLAGS_IN_STRUCT) || FLAG_ISSET(nextFlag, FLAGS_IN_UNNAMEDSTRUCT)) {
-                        D3(debug("-- adding member %s of type %s to structure %s...\n", member_name, unnamed_struct_def->name().c_str(), ((structure_definition*)pConstruct)->name().c_str()));
+                        D(debug("-- adding member %s of type %s to structure %s...\n", member_name, unnamed_struct_def->name().c_str(), ((structure_definition*)pConstruct)->name().c_str()));
                         if (FLAG_ISSET(flags, FLAGS_IN_STRUCT) || FLAG_ISSET(flags, FLAGS_IN_UNNAMEDSTRUCT)) {
                             ((structure_definition*)pConstruct)->add_member(member_name, unnamed_struct_def->name().c_str());
                         } else if (FLAG_ISSET(flags, FLAGS_IN_UNION) || FLAG_ISSET(flags, FLAGS_IN_UNNAMEDUNION)) {
                             ((structure_definition*)pConstruct)->add_union_member(member_name, unnamed_struct_def->name().c_str());
                         }
                     } else if (FLAG_ISSET(nextFlag, FLAGS_IN_UNION) || FLAG_ISSET(nextFlag, FLAGS_IN_UNNAMEDUNION)) {
-                        D3(debug("-- adding member %s of type %s to union %s...\n", member_name, unnamed_struct_def->name().c_str(), ((union_definition*)pConstruct)->name().c_str()));
+                        D(debug("-- adding member %s of type %s to union %s...\n", member_name, unnamed_struct_def->name().c_str(), ((union_definition*)pConstruct)->name().c_str()));
                         ((union_definition*)pConstruct)->add_member(member_name, unnamed_struct_def->name().c_str());
                     }
                 } else {
@@ -627,7 +627,7 @@ bool structure_database::parse_file(const char* filename) {
 
                         member_index_size = calculate_array_size(member_index);
 
-                        D3(debug("-- STRUCTURE ARRAY %s TYPE %s SIZE %d!\n", member_name, member_type, member_index_size));
+                        D(debug("-- STRUCTURE ARRAY %s TYPE %s SIZE %d!\n", member_name, member_type, member_index_size));
 
                         if ((flags & FLAGS_IN_STRUCT) || (flags & FLAGS_IN_UNNAMEDSTRUCT)) {
                             assert(pConstruct != 0L);
@@ -654,7 +654,7 @@ bool structure_database::parse_file(const char* filename) {
                         member_name[len] = 0;
                         strncpy(member_name, lineptr + matches[2].rm_so, len);
 
-                        D3(debug("STRUCTURE MEMBER type %s name %s!\n", member_type, member_name));
+                        D(debug("STRUCTURE MEMBER type %s name %s!\n", member_type, member_name));
 
     //	--	Add this member to the current structure...
 
@@ -671,7 +671,7 @@ bool structure_database::parse_file(const char* filename) {
                         structure_definition*		pStruct = 0;
 
                         get_next_unnamed_struct(structName);
-                        D3(debug("Encountered unnamed struct [%s]!\n", structName.c_str()));
+                        D(debug("Encountered unnamed struct [%s]!\n", structName.c_str()));
                         pStruct = new_structure( structName.c_str() );
                         push_state(FLAGS_IN_UNNAMEDSTRUCT, pStruct);
 //                        get_unnamed_struct();
@@ -681,7 +681,7 @@ bool structure_database::parse_file(const char* filename) {
 
                         get_next_unnamed_union(unionName);
 
-                        D3(debug("Encountered unnamed union %s!\n", unionName.c_str()));
+                        D(debug("Encountered unnamed union %s!\n", unionName.c_str()));
                         pUnion = new_union( unionName.c_str() );
 
                         push_state(FLAGS_IN_UNNAMEDUNION, pUnion);
@@ -696,7 +696,7 @@ bool structure_database::parse_file(const char* filename) {
                         member_name[len] = 0;
                         strncpy(member_name, lineptr + matches[2].rm_so, len);
 
-                        D3(debug("ENUM MEMBER type %s name %s!\n", member_type, member_name));
+                        D(debug("ENUM MEMBER type %s name %s!\n", member_type, member_name));
 
     //	--	Add this member to the current structure...
 
@@ -734,7 +734,7 @@ bool structure_database::parse_file(const char* filename) {
 
                         //printf("size %d\n", member_index_size);
 
-                        D3(debug("ARRAY MEMBER %s TYPE %s SIZE [%s] = %d!\n", member_name, member_type, member_index, member_index_size));
+                        D(debug("ARRAY MEMBER %s TYPE %s SIZE [%s] = %d!\n", member_name, member_type, member_index, member_index_size));
 
                         if ((flags & FLAGS_IN_STRUCT) || (flags & FLAGS_IN_UNNAMEDSTRUCT)) {
                             assert(pConstruct != 0L);
@@ -754,7 +754,7 @@ bool structure_database::parse_file(const char* filename) {
                         member_name[len] = 0;
                         strncpy(member_name, lineptr + matches[2].rm_so, len);
 
-                        D3(debug("MEMBER %s TYPE %s\n", member_name, member_type));
+                        D(debug("MEMBER %s TYPE %s\n", member_name, member_type));
 
                         if ((flags & FLAGS_IN_STRUCT) || (flags & FLAGS_IN_UNNAMEDSTRUCT)) {
                             assert(pConstruct != 0L);
@@ -765,7 +765,7 @@ bool structure_database::parse_file(const char* filename) {
                         }
                     } else if (regexec(&exp_memb_union, lineptr, 3, matches, 0) == 0) {
                         size_t	len = matches[1].rm_eo - matches[1].rm_so;
-                        D3(debug("FOUND UNION MEMBER!\n"));
+                        D(debug("FOUND UNION MEMBER!\n"));
 
                         member_type[len] = 0;
                         strncpy(member_type, lineptr + matches[1].rm_so, len);
@@ -774,7 +774,7 @@ bool structure_database::parse_file(const char* filename) {
                         member_name[len] = 0;
                         strncpy(member_name, lineptr + matches[2].rm_so, len);
 
-                        D3(debug("MEMBER %s TYPE %s\n", member_name, member_type));
+                        D(debug("MEMBER %s TYPE %s\n", member_name, member_type));
                         if ((flags & FLAGS_IN_STRUCT) || (flags & FLAGS_IN_UNNAMEDSTRUCT)) {
                             assert(pConstruct != 0L);
                             ((structure_definition*)pConstruct)->add_member(member_name, member_type);
@@ -783,7 +783,7 @@ bool structure_database::parse_file(const char* filename) {
                             ((union_definition*)pConstruct)->add_member(member_name, member_type);
                         }
                     } else {
-                        D3(debug("UNKNOWN %s", lineptr));
+                        D(debug("UNKNOWN %s", lineptr));
                         //printf("%s\n", lineptr);
                     }
 
@@ -841,11 +841,20 @@ void structure_database::dump_html_table(const char* szFileName, const char* szH
     if (szHeading != 0) {
         fprintf(pFP,"<h2>%s</h2>\n", szHeading);
     }
-    fprintf(pFP, "</center>\n<ul>\n");
+    fprintf(pFP, "</center>\n");
+    fprintf(pFP, "<h3>Structures</h3>\n");
+	fprintf(pFP, "<ul>\n");
 	for (size_t i = 0 ; i < size() ; i++) {
 		const structure_definition*	pDef = (*this)[i];
         fprintf(pFP, "\t<li><a href=\"#%s\">%s</a></li>\n", pDef->name().c_str(),pDef->name().c_str());
 	}
+    fprintf(pFP, "</ul>\n");
+    fprintf(pFP, "<h3>Unions</h3>\n");
+    fprintf(pFP, "<ul>\n");
+    for (size_t i = 0 ; i < union_vector.size() ; i++) {
+        const union_definition* pUnion = union_vector[i];
+        fprintf(pFP, "\t<li><a href=\"#%s\">%s</a></li>\n", pUnion->name().c_str(),pUnion->name().c_str());
+    }
     fprintf(pFP, "</ul>\n");
     fprintf(pFP, "<hr>\n");
 	for (size_t i = 0 ; i < size() ; i++) {
@@ -878,6 +887,36 @@ void structure_database::dump_html_table(const char* szFileName, const char* szH
         fprintf(pFP, "</table><br>\n");
 	}
 
+	fprintf(pFP, "<hr>\n");
+	for (size_t i = 0 ; i < union_vector.size() ; i++) {
+		const union_definition*	pDef = union_vector[i];
+
+        fprintf(pFP, "<a name=\"%s\" /><table border='1'>\n\t<caption>%s</caption>\n", pDef->name().c_str(), pDef->name().c_str());
+        fprintf(pFP, "\t<tr><th>Offset</th><th>Type</th><th>Name</th></tr>\n");
+
+        if (pDef->member_count() > 0) {
+            for (int j = 0 ; j < pDef->member_count() ; j++) {
+                const member_definition*	pMemb = pDef->member(j);
+
+                if (pMemb->is_array()) {
+                    char	sTmp[1024];
+                    snprintf(sTmp, 1024, "%s[%ld]", pMemb->name().c_str(), (long)pMemb->maxindex());
+
+                    fprintf(pFP, "\t<tr><td>%ld</td><td>%s</td><td>%s</td></tr>\n",
+                        (long)pMemb->offset(),
+                        pMemb->type().c_str(),
+                        sTmp);
+                } else {
+                    fprintf(pFP, "\t<tr><td>%ld</td><td>%s</td><td>%s</td></tr>\n",
+                        (long)pMemb->offset(),
+                        pMemb->type().c_str(),
+                        pMemb->name().c_str());
+                }
+            }
+        }
+        fprintf(pFP, "\t<tr><td colspan=\"2\">Total :</td><td>%ld</td></tr>\n", (long)pDef->size());
+        fprintf(pFP, "</table><br>\n");
+	}
     fprintf(pFP, "</body>\n</html>\n");
 
     fclose(pFP);
