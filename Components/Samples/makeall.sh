507292a57a76b9b45e23b1d248ed66be59ea584f
Michael Uman <Michael_Uman@sigmadesigns.com>
2016-08-11 15:57:50 -0700

    Check in shared object link scripts for smpPlatformLib and testEngine.
    * Added testEngine to the Samples makeall.sh script.
    * testEngine makefile supports both static and shared build targets.
    * Added SHARED to LINK_ARRAY in buildoptions.
    * Build shared and static smpPlatformLib libraries.
    * Toplevel makeall script now builds all LINK_ARRAY types.

--- Components/Samples/makeall.sh
+++ Components/Samples/makeall.sh
@@ -3,7 +3,7 @@
 #	Build all components
 ###############################################################################
 
-COMPONENTS="testgbus testPlatformDB testrc testStruct testsymbols testAlloc"
+COMPONENTS="testgbus testPlatformDB testrc testStruct testsymbols testAlloc testEngine" 
 
 for comp in $COMPONENTS; do
 	pushd > /dev/null $comp
--- scripts/makeall.sh
+++ scripts/makeall.sh
@@ -23,6 +23,8 @@ if [ -t 1 ]; then
 	clear
 fi
 
+LINK_ARRAY=( 'STATIC=1' )
+
 if [ -f buildoptions ]; then
 . buildoptions
 else
@@ -31,16 +33,17 @@ ARCH_ARRAY=( 'ARCH=x86_64' 'ARCH=x86' )
 DBG_ARRAY=( 'NDEBUG=1' 'DEBUG=1')
 fi
 
-for dbg in ${DBG_ARRAY[@]}; do
-	for arch in ${ARCH_ARRAY[@]}; do
-		if [ "$CLEAN" == "1" ]; then
-			make 2>&1 $arch $dbg clean
-			if [ -n "${TOUCH_FILES}" ]; then
-				touch_files "${TOUCH_FILES[@]}"
+for link in ${LINK_ARRAY[@]}; do
+	for dbg in ${DBG_ARRAY[@]}; do
+		for arch in ${ARCH_ARRAY[@]}; do
+			if [ "$CLEAN" == "1" ]; then
+				make 2>&1 ${arch} ${dbg} ${link} clean
+				if [ -n "${TOUCH_FILES}" ]; then
+					touch_files "${TOUCH_FILES[@]}"
+				fi
 			fi
-		fi
-		make 2>&1 $arch $dbg
+			make 2>&1 ${arch} ${dbg} ${link}
+		done
 	done
 done
-
 
