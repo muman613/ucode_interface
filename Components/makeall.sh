126d5966d8b41d56b8dd4d87ae1c61c2a7f903c7
Michael Uman <Michael_Uman@sigmadesigns.com>
2016-08-30 16:56:43 -0700

    Add targetOptions to components built

--- Components/makeall.sh
+++ Components/makeall.sh
@@ -3,7 +3,7 @@
 #	Build all components
 ###############################################################################
 
-COMPONENTS="gbusfifolib platformDB platformDB2 remote_client structure_control symbolmgr fileresolver smpPlatformLib"
+COMPONENTS="gbusfifolib platformDB platformDB2 remote_client structure_control symbolmgr fileresolver smpPlatformLib targetOptions"
 
 for comp in $COMPONENTS; do
 	pushd > /dev/null $comp
